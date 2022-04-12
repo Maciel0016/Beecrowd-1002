731ce1df19272ae08ae49ee3a7696daddfde307f
kidoc <kidoc@DESKTOP-6T3Q9GP>
2022-04-11 22:54:53 -0300

    Add project files.

---
+++ Beecrowd area do circulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+ class program
+{
+    static void Main(string[] args)
+    {
+        double a , raio, n = 3.14159;
+
+
+        raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        a = Math.Pow(raio, 2.0) * n;
+
+   
+
+        Console.WriteLine("A=" + a.ToString("F4",CultureInfo.InvariantCulture));
+        Console.ReadLine();
+    }
+}
+
